962b2ddb7a4966d44b4df7469f8f92ba80bf290b
Ryan Van Etten <ryanve@gmail.com>
2016-09-07 01:59:08 -0700

    add closure compiler annotation

--- speculative.js
+++ speculative.js
@@ -1,13 +1,16 @@
 !function(root, name, make) {
   typeof module != 'undefined' && module.exports ? module.exports = make() : root[name] = make()
 }(this, 'speculative', function() {
-  
+
   function inject(tap) {
     var link = document.createElement('link')
     tap(link)
     document.head.appendChild(link)
   }
 
+  /**
+   * @param {{rel: string, href: string}} atts
+   */
   function speculative(atts) {
     inject(function(link) {
       for (var k in atts) {
